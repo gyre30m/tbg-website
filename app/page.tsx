8e1423d49800b8ca0a0aa7bf1890f42ee676b59c
gyre30m <bradley@thegyre.io>
2025-03-17 19:55:03 -0500

    new cover art

--- app/page.tsx
+++ app/page.tsx
@@ -73,6 +73,14 @@ export default function Home() {
             <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
+                  <Image
+                    className="dark:invert mx-auto mb-6"
+                    src="/tbg-logo.svg"
+                    alt="The Bradley Group logo"
+                    width={300}
+                    height={200}
+                    priority
+                  />
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
                     Forensic Economists
                   </h1>
@@ -513,79 +521,8 @@ export default function Home() {
             </div>
             <div className="mx-auto grid max-w-5xl grid-cols-1 gap-6 py-12 lg:grid-cols-2">
               <div className="flex flex-col gap-4">
-                <h3 className="text-xl font-bold">Contact Information</h3>
+                <h3 className="text-xl font-bold">Contact Us</h3>
                 <div className="grid gap-2">
-                  <p className="flex items-center gap-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="h-5 w-5 text-primary"
-                    >
-                      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-                    </svg>
-                    <span>(650) 440-2417</span>
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="h-5 w-5 text-primary"
-                    >
-                      <rect width="20" height="16" x="2" y="4" rx="2" />
-                      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-                    </svg>
-                    <span>
-                      <Link
-                        className="underline text-blue-600 hover:text-blue-800"
-                        href="mailto:info@the-bradley-group.com"
-                      >
-                        info@the-bradley-group.com
-                      </Link>
-                    </span>
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="h-5 w-5 text-primary"
-                    >
-                      <path d="M8 2v4" />
-                      <path d="M16 2v4" />
-                      <rect width="18" height="18" x="3" y="4" rx="2" />
-                      <path d="M3 10h18" />
-                      <path d="M10 16h4" />
-                      <path d="M12 14v4" />
-                    </svg>
-                    <span>
-                      <Link
-                        className="underline text-blue-600 hover:text-blue-800"
-                        href="https://calendar.notion.so/meet/bradleygibbs/9992k4lze"
-                      >
-                        Notion Scheduler
-                      </Link>
-                    </span>
-                  </p>
                   <p className="flex items-center gap-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -611,7 +548,49 @@ export default function Home() {
                         className="underline text-blue-600 hover:text-blue-800"
                         href="https://meetings-na2.hubspot.com/brad-gibbs"
                       >
-                        Hubspot Scheduler
+                        Schedule a video call
+                      </Link>
+                    </span>
+                  </p>
+                  <p className="flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="h-5 w-5 text-primary"
+                    >
+                      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                    </svg>
+                    <span>(608) 904-8690</span>
+                  </p>
+                  <p className="flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="h-5 w-5 text-primary"
+                    >
+                      <rect width="20" height="16" x="2" y="4" rx="2" />
+                      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                    </svg>
+                    <span>
+                      <Link
+                        className="underline text-blue-600 hover:text-blue-800"
+                        href="mailto:info@the-bradley-group.com"
+                      >
+                        info@the-bradley-group.com
                       </Link>
                     </span>
                   </p>
