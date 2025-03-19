046e8868dca21f4e1add20777cbe4744d436cb80
gyre30m <bradley@thegyre.io>
2025-03-18 20:29:40 -0500

    typescript that shit up

--- components/ui/contact-form.tsx
+++ components/ui/contact-form.tsx
@@ -1,31 +1,43 @@
-/**
- * The HubSpotForm component is a React component that creates a HubSpot form and renders it in a
- * specified target element.
- *
- * The portalId and formId can be obtained from the form embed code within hubspot.
- *
- * @returns The component is returning a `<div>` element with the id "hubspotForm" and the className
- * "hubspotForm".
- */
-import { useEffect } from "react";
+"use client";
+import React, { useEffect } from "react";
 
-export default function HSContactForm() {
+interface HubspotFormProps {
+  region: string;
+  portalId: string;
+  formId: string;
+}
+
+interface WindowWithHbspt extends Window {
+  hbspt: any;
+}
+
+const HubspotForm: React.FC<HubspotFormProps> = ({
+  region,
+  portalId,
+  formId,
+}) => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://js.hsforms.net/forms/v2.js";
-    // script.src = "https://js-na2.hsforms.net/forms/embed/242272349.js";
+    script.src = "https://js.hsforms.net/forms/shell.js";
     document.body.appendChild(script);
 
     script.addEventListener("load", () => {
-      if (window.hbspt) {
-        window.hbspt.forms.create({
-          portalId: "242272349",
-          formId: "162605ae-c2b0-4771-ab7c-13bbd5917469",
+      if ((window as unknown as WindowWithHbspt).hbspt) {
+        (window as unknown as WindowWithHbspt).hbspt.forms.create({
+          region: region,
+          portalId: portalId,
+          formId: formId,
           target: "#hubspotForm",
         });
       }
     });
-  }, []);
 
-  return <div id="hubspotForm" className="hubspotForm"></div>;
-}
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [region, portalId, formId]);
+
+  return <div id="hubspotForm"></div>;
+};
+
+export default HubspotForm;
