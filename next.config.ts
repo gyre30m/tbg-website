0c6610c096e2e26070b7df564e9fbe1176242393
gyre30m <bradley@thegyre.io>
2025-07-01 14:46:00 -0500

    fix Supabase dependenchy issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      (warning: any) =>
+        warning.message.includes('Critical dependency') &&
+        warning.message.includes('realtime-js')
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
